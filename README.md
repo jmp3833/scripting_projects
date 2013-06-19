30eb96ca7660fb5a5992f70c92a4ea135e2aa1a5
user <user@warbler.rit.edu>
2013-06-19 13:44:26 -0400

    Initial commit, includes gravStreamsUpdater and timelapse-encode

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 scripting_projects
 ==================
 
-Several mini projects and solutions that I've done through scripting in different languages
+Several mini projects and solutions made using various languages such as ruby and bash
+
+Projects currently in this repository:
+
+=============================================================
+
+gravStreamsUpdater: Contains a separate readme file in its directory
+
+timelapse-encode: Small utility package made to remove frames 
+from a timelapse that arent up to quality standards, and to
+encode individual frames into a .mp4 timelapse file. 
+
+=============================================================
---
+++ timelapse-encode/timelapse-encode-poli1.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#Creates a timelapse of the SAU 1 cam pointing at the Polisseni Center
+
+#Check if user is root
+#Make sure only root can run our script
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+#ruby detect-bad-timelapse-framesp1.rb
+
+#Generates a filename for the final video
+FILENAME="poli1-timelapse-`date +%Y%m%d`"
+
+#produces a .h264 file from various.png images
+mencoder "mf:///mnt/icelab-timelapse/SAU-timelapse-daily/SAU_233.17.33.206.50004_*/*.png" -mf type=png:fps=30 -ovc x264 -of lavf -o /mnt/icelab-videos/SAU-timelapse-videos/$FILENAME.h264
+
+#Converts the .h264 file into a .mp4 file
+ffmpeg -i /mnt/icelab-videos/SAU-timelapse-videos/$FILENAME.h264 /mnt/icelab-videos/SAU-timelapse-videos/$FILENAME.mp4
+
+#Removes the old .h264 file
+rm /mnt/icelab-videos/SAU-timelapse-videos/$FILENAME.h264
+
